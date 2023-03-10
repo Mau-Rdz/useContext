5ff1aacbcf541f3b3fe1b4217efb3a63a0e4fba3
MauRdzP <mauricio.r@cogentibs.in>
2023-03-09 18:46:12 -0600

    Css and titles added

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -2,24 +2,44 @@
 import React, { useContext } from "react";
 import { UserContext } from "../userContext.ts";
 import { users } from "../../Interfaces/users";
-import './Card.css'
+import "./Card.css";
 
 function Card() {
   const { id, name, url, job, birth, joined, expire, web, phone } =
     useContext<users>(UserContext);
 
   return (
-  <div className="CardContainer">
-    <div>
-        <img src={url}/>
+    <div className="CardContainer">
+      <div>
+        <img src={url} alt="Foto user" />
+      </div>
+      <div className="Name">{name}</div>
+      <div className="Job">{job}</div>
+      <div>
+        <p className="titulo">ID No:</p>
+        {id}
+      </div>
+      <div>
+        <p className="titulo">DOB.</p>
+        {birth}
+      </div>
+      <div>
+        <p className="titulo">Joined Date</p>
+        {joined}
+      </div>
+      <div>
+        <p className="titulo">Expire Date</p>
+        {expire}
+      </div>
+      <div>
+        <p className="titulo">Web</p>
+        {web}
+      </div>
+      <div>
+        <p className="titulo">Phone</p>
+        {phone}
+      </div>
     </div>
-    <div className="Name">
-        {name}
-    </div>
-    <div className="Job">
-        {job}
-    </div>
-  </div>
   );
 }
 
